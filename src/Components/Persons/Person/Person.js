96ed8099f50fe812de45dce44e6acc7ea4503e89
parthasarathi <me.parthu@gmail.com>
2020-07-02 19:19:33 +0530

    Use Ref

--- src/Components/Persons/Person/Person.js
+++ src/Components/Persons/Person/Person.js
@@ -6,16 +6,32 @@ import Aux from '../../../hoc/Auxilary';
 import withClass from '../../../hoc/withClass';
 
 class Person extends Component {
+
+  //another way to use Ref
+  constructor(props){
+    super(props);
+    //older approch for ref
+    this.inputElementRef = React.createRef();
+
+  }
+  componentDidMount(){
+    //new approch
+  //  this.inputElement.focus();
+  //old approch
+    this.inputElementRef.current.focus();
+  }
   render(){
   console.log('[person.js] rendering...');
-
     return (
       <Aux>
         <p onClick={this.props.click}>
           i am from Person {this.props.name} and {this.props.age}
         </p>
         <h2>{this.props.children}</h2>
-        <input type="text" onChange={this.props.changed} value={this.props.name} />
+        <input type="text" 
+        //ref = {(inputEl) => this.inputElement = inputEl} 
+        ref = {this.inputElementRef}
+        onChange={this.props.changed} value={this.props.name} />
       </Aux>
     );
   }
